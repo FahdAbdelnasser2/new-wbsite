7f9f87a2653ddccdfc36b28cea20bcc833274f5f
fady <zeyad@raitotec.com>
2025-09-22 09:48:07 +0300

    ......

--- assets/js/newJs.js
+++ assets/js/newJs.js
@@ -903,3 +903,21 @@ document.querySelectorAll(".btn-primary, .btn-secondary").forEach((btn) => {
           }
         });
       });
+        const swiper = new Swiper(".digi-logo-slider", {
+    loop: true, // يكرر العرض
+    slidesPerView: 4, // عدد اللوجوهات في الشاشة
+    spaceBetween: 30, // مسافة بين العناصر
+    autoplay: {
+      delay: 2000, // يقلب كل ثانيتين
+      disableOnInteraction: false, // يفضل شغال حتى لو المستخدم قلب يدوي
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      320: { slidesPerView: 2, spaceBetween: 10 },
+      768: { slidesPerView: 3, spaceBetween: 20 },
+      1024: { slidesPerView: 5, spaceBetween: 30 },
+    },
+  });
